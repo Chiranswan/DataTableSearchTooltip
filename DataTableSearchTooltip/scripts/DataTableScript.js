a46a425b9c06ff1e9e12b2ef3726bb47327d7690
Chiran <swan.chiran@gmail.com>
2017-09-09 15:41:11 +0530

    Initial Release

---
+++ DataTableSearchTooltip/scripts/DataTableScript.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    var table = $('#example').DataTable({
+        paging: false,
+
+        ordering: false,
+
+    });
+
+    $('#search-inp').on('keyup', function () {
+        table.search($(this).val()).draw();
+    });
+
+    $('#search-inp').on('blur', function () {
+        var SearchVal = $(this).val();
+        $('#Div_Search').attr("data-tip", SearchVal);
+    });
+
+});
